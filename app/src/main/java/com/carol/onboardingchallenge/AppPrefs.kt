64af758042f2e7d2fd50c8369c59001ff30e999b
carolinemusyoka <carolmusyoka127@gmail.com>
2020-07-10 13:32:41 +0300

    Adjusing views and optimizing imports

--- app/src/main/java/com/carol/onboardingchallenge/AppPrefs.kt
+++ app/src/main/java/com/carol/onboardingchallenge/AppPrefs.kt
@@ -5,8 +5,8 @@ import android.content.SharedPreferences
 
 class AppPrefs(context: Context) {
 
-    private lateinit var prefs: SharedPreferences
-    private lateinit var prefsEditor: SharedPreferences.Editor
+    private var prefs: SharedPreferences
+    private var prefsEditor: SharedPreferences.Editor
 
     init {
         prefs = context.getSharedPreferences(PREF_NAME, PRIVATE_MODE)
@@ -17,8 +17,6 @@ class AppPrefs(context: Context) {
         prefsEditor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime)
         prefsEditor.commit()
     }
-
-    fun isFirstTimeLaunch(): Boolean = prefs.getBoolean(IS_FIRST_TIME_LAUNCH, true)
 
     companion object {
         private const val PRIVATE_MODE = 0    // Shared preference mode
--- app/src/main/java/com/carol/onboardingchallenge/extensions.kt
+++ app/src/main/java/com/carol/onboardingchallenge/extensions.kt
@@ -1,14 +1,6 @@
 package com.carol.onboardingchallenge
 
-import android.content.Context
-import android.os.Handler
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-
-fun Context.toast(message: String) {
-
-}
 
 fun View.show() {
     visibility = View.VISIBLE
